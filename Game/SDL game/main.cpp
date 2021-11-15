b78cbb694914662520bd551f9430249e4494b22e
BozhidarBoaydzhiev <bpboyadziev20@codingburgas.bg>
2021-11-15 18:01:14 +0200

    Add box collision and screen collision (border)

--- Game/SDL game/main.cpp
+++ Game/SDL game/main.cpp
@@ -127,6 +127,50 @@ void text() {
 	SDL_DestroyTexture(Message);
 }
 
+bool checkCollision(SDL_Rect a, SDL_Rect b)
+{
+	//The sides of the rectangles
+	int leftA, leftB;
+	int rightA, rightB;
+	int topA, topB;
+	int bottomA, bottomB;
+
+	//Calculate the sides of rect A
+	leftA = a.x;
+	rightA = a.x + a.w;
+	topA = a.y;
+	bottomA = a.y + a.h;
+
+	//Calculate the sides of rect B
+	leftB = b.x;
+	rightB = b.x + b.w;
+	topB = b.y;
+	bottomB = b.y + b.h;
+	//If any of the sides from A are outside of B
+	if (bottomA <= topB)
+	{
+		return false;
+	}
+
+	if (topA >= bottomB)
+	{
+		return false;
+	}
+
+	if (rightA <= leftB)
+	{
+		return false;
+	}
+
+	if (leftA >= rightB)
+	{
+		return false;
+	}
+
+	//If none of the sides from A are outside B
+	return true;
+}
+
 int main(int argc, char* argv[])
 {
 
@@ -148,6 +192,12 @@ int main(int argc, char* argv[])
 			texr.x = x; texr.y = y; texr.w = xSize; texr.h = ySize;
 			SDL_Event event;
 
+			//Set the wall
+			SDL_Rect wall{};
+			wall.x = 90;
+			wall.y = 40;
+			wall.w = 40;
+			wall.h = 400;
 			while (!quit)
 			{
 				//Creating the golf ball
@@ -185,65 +235,111 @@ int main(int argc, char* argv[])
 							oldxMouse = xMouse;
 							oldyMouse = yMouse;
 
-							//Changing the player coordinates
-							if ((xMouse <= 500 && xMouse >= 300) || yMouse <= 240) {
-								texr.x = xMouse / 2;
-								texr.y = yMouse * 2;
-							}
-							else {
-								texr.x = xMouse * 2;
-								texr.y = yMouse / 2;
-							}
+							////Changing the player coordinates
+							//if ((xMouse <= 500 && xMouse >= 300) || yMouse <= 240) {
+							//	texr.x = xMouse / 2;
+							//	texr.y = yMouse * 2;
+							//}
+							//else {
+							//	texr.x = xMouse * 2;
+							//	texr.y = yMouse / 2;
+							//}
 
-							if (x == 640) {
-								texr.x = xMouse / 2;
-							}
-							else if (y == 480) {
-								texr.y = yMouse / 2;
-							}
+							//if (x == 640) {
+							//	texr.x = xMouse / 2;
+							//}
+							//else if (y == 480) {
+							//	texr.y = yMouse / 2;
+							//}
 						}
 						//Move with w,a,s,d
-						//switch (event.key.keysym.sym)
-						//{
-						//case SDLK_SPACE:
-						//	//if (y > ground.y - ySize)
-						//	//	gravity = -0.2f * jumpHeight;
-						//	//break;
-						//case SDLK_a:
-						//if (x > 0)
-						//	texr.x -= 5;
-						//	break;
-						//case SDLK_d:
-						//	texr.x += 5;
-						//	break;
-						//case SDLK_s:
-						//	texr.y += 5;
-						//	break;
-						//case SDLK_w:
-						//	texr.y -= 5;
-						//	break;
-						//}
+						int mVelX = 0, mVelY = 0;
+							if (event.type == SDL_KEYDOWN)
+							{
+								//Adjust the velocity
+								switch (event.key.keysym.sym)
+								{
+									case SDLK_w: texr.y -= 5; break;
+									case SDLK_s: texr.y += 5; break;
+									case SDLK_a: texr.x -= 5; break;
+									case SDLK_d: texr.x += 5; break;
+								}
+							}
+
+							SDL_Rect mCollider;
+							mCollider.w = xSize;
+							mCollider.h = ySize;
+
+							texr.x -= mVelX;
+							mCollider.x = texr.x;
+
+							//If the dot collided or went too far to the left or right
+							if ((texr.x < 0 || texr.x > SCREEN_WIDTH || (texr.x + mCollider.w > SCREEN_WIDTH) || checkCollision(mCollider, wall)) && SDL_KEYUP)
+							{
+								switch (event.key.keysym.sym) {
+								case SDLK_a:
+									//Move back
+									texr.x += 5;
+									mCollider.x = texr.x;
+									break;
+								case SDLK_d:
+									//Move back
+									texr.x -= 5;
+									mCollider.x = texr.x;
+									break;
+								default:
+									break;
+								}
+							}
+
+							//Move the dot up or down
+							texr.y -= mVelY;
+							mCollider.y = texr.y;
+
+							//If the dot collided or went too far up or down
+							if ((texr.y < 0 || texr.y > SCREEN_HEIGHT || (texr.y + mCollider.h > SCREEN_HEIGHT) || checkCollision(mCollider, wall)) && SDL_KEYUP)
+							{
+								switch (event.key.keysym.sym) {
+								case SDLK_s:
+									//Move back
+									texr.y -= 5;
+									mCollider.y -= 5;
+									break;
+								case SDLK_w:
+									//Move back
+									texr.y += 5;
+									mCollider.y -= 5;
+									break;
+								}
+							}
+
+							/**/
+						//Initializing the player's new coordinates
+						SDL_RenderCopy(renderer, player, NULL, &texr);
 					}
-					//Initializing the player's new coordinates
-					SDL_RenderCopy(renderer, player, NULL, &texr);
+
+					//Render wall
+					SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+					SDL_RenderDrawRect(renderer, &wall);
+
+
+					//Calling the text function
+					//TTF_Init();
+					//text();
+
+					//Middle horizontal line
+					//SDL_UpdateWindowSurface(window);
+					//SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+					//SDL_RenderDrawLine(renderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2);
+
+					SDL_RenderPresent(renderer);
 				}
-
-				//Calling the text function
-				TTF_Init();
-				text();
-
-				//Middle horizontal line
-				//SDL_UpdateWindowSurface(window);
-				//SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
-				//SDL_RenderDrawLine(renderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2);
-
-				SDL_RenderPresent(renderer);
 			}
 		}
-	}
-	//Closing everything
-	TTF_Quit();
-	close();
+		//Closing everything
+		TTF_Quit();
+		close();
 
-	return 0;
+		return 0;
+	}
 }
